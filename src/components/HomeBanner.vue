00a275ac81c0ffc4dffbd16fc218eefc733e8411
Pyro64 <trifonov.roman64@gmail.com>
2022-06-16 13:36:58 +0400

    bue ws

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="banner">
+    <div class="banner__block">
+      <div class="banner__text">Готовы предложить</div>
+      <div class="banner__title">Готовы предложить</div>
+      <div class="banner__text">Быстро и качественно</div>
+      <button>Заказать</button>
+    </div>
+    <img
+      class="banner__img"
+      src="../assets/images/home-banner.png"
+      alt="banner"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../assets/styles/mixins.scss";
+
+.banner {
+  @include container;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,11 @@
-<template><div></div></template>
+<template></template>
 
 <script>
-import { useMouse, useToggle } from "@vueuse/core";
-import { storeToRefs } from "pinia";
-import { useCounterStore } from "../stores/counter";
-export default {
-  setup() {
-    const { x, y } = useMouse();
-    const [value, toggle] = useToggle();
-    const store = storeToRefs(useCounterStore());
-    const { counter } = store;
-    const { increment } = useCounterStore();
-    return {
-      increment,
-      counter,
-      x,
-      y,
-      value,
-      toggle,
-    };
-  },
-};
+import HomeBanner from "../components/HomeBanner.vue";
 </script>
 <style lang="scss" scoped>
 @import "../assets/styles/mixins.scss";
+
 .wrapper {
   @include fluid(margin-top, 15px, 25px);
   @include container;
